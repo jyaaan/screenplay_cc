f82118053a263dc83a4c5f7264c21f22782c7402
jyaaan <john.yamashiro@gmail.com>
2017-12-02 22:23:03 -0800

    adding selector and map function to iterate through all links on page #2

---
+++ index.js
@@ -0,0 +1,10 @@
+const targetURLs = [
+  'https://screenplay.thinkific.com/enroll/219920',
+  'https://screenplay.thinkific.com/cart/add_product/219920?price_id=225328'
+]
+
+const $a = document.querySelectorAll('a');
+
+$a.map(a => {
+  console.log(a.href);
+});
