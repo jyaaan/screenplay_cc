321e0a67de6afd4c2e266edb91545130d4f8f138
jyaaan <john.yamashiro@gmail.com>
2017-12-02 22:26:39 -0800

    adding inbound url to console #2

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const targetURLs = [
   'https://screenplay.thinkific.com/cart/add_product/219920?price_id=225328'
 ]
 
+const inboundURL = window.location.href;
 const $a = document.querySelectorAll('a');
 
 $a.map(a => {
   console.log(a.href);
+  console.log('inbound URL: ' + inboundURL);
 });
